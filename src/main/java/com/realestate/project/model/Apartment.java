bb95235773ddc3e1ec63cb466eb1fb1dbf4453b1
Kalpaaa07 <it25102023@my.sliit.lk>
2026-04-22 06:45:59 +0530

    Fix Apartment entity with inheritance

--- src/main/java/com/realestate/project/model/Apartment.java
+++ src/main/java/com/realestate/project/model/Apartment.java
@@ -1,4 +1,31 @@
 package com.realestate.project.model;
 
-public class Apartment {
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Apartment extends Property {
+
+    private int floorNumber;
+
+    public Apartment(String title, String location, double price, String description, int floorNumber){
+        super(title,location,price,description);
+        this.floorNumber = floorNumber;
+
+    }
+
+    @Override
+    public String getPropertyCategory(){
+        return "Apartment";
+    }
 }
+
+
+
