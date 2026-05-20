4b67491ca6af0c7bd4706f3dcb57852be0f85193
IT25102036HeshanWRAA <it25102036heshanwraa@gmail.com>
2026-05-20 11:48:49 +0530

    new

--- src/main/java/com/realestate/project/repo/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.realestate.project.repo;
-
-import com.realestate.project.entity.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
